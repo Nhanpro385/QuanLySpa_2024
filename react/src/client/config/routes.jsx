20c663324d17f4a906a58a6956d919ec14dd0d35
tranphihao2003 <113425723+tranphihao2003@users.noreply.github.com>
2024-09-30 11:03:23 +0700

    FE-002	[CODE]-Booking Page

--- react/src/client/config/routes.jsx
+++ react/src/client/config/routes.jsx
@@ -1,12 +1,12 @@
 import Home from "../pages/Home";
-// import Appbooking from "../pages/Booking";
+import Appbooking from "../pages/Booking";
 // import BookingInfo from "../pages/BookingInfo";
 // import Pricingpage from "../pages/Pricingpage";
 // import VideoCall from "../pages/VideoCall";
 
 const PublicRoutes = [
     { path: "/", element: <Home /> },
-    // { path: "/booking", element: <Appbooking /> },
+    { path: "/booking", element: <Appbooking /> },
     // { path: "/bookinginfo", element: <BookingInfo /> },
     // { path: "/pricing", element: <Pricingpage /> },
     // { path: "/videocall", element: <VideoCall /> },
