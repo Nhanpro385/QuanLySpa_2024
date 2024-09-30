e4810d5a1dad128131c736ce80ce29c9d9cddb24
kiet200116 <kietdragonpro17@gmail.com>
2024-10-01 00:37:58 +0700

    BE-008 [CODE]-Controller bảng categories(admin) BE-009 [CODE]-Controller bảng supliers(admin)

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,32 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\SupplierController;
+use App\Http\Controllers\Admin\CategoryController;
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::middleware('auth:api')->group(function () {
+    Route::prefix('admin/suppliers')->group(function () {
+        Route::get('/', [SupplierController::class, 'index'])->name('suppliers.index');
+        Route::post('/', [SupplierController::class, 'store'])->name('suppliers.store');
+        Route::get('/{id}', [SupplierController::class, 'show'])->name('suppliers.show');
+        Route::put('/{id}', [SupplierController::class, 'update'])->name('suppliers.update');
+        Route::delete('/{id}', [SupplierController::class, 'destroy'])->name('suppliers.destroy');
+    });
+});
+
+
+Route::middleware('auth:api')->group(function () {
+    Route::prefix('admin/categories')->group(function () {
+        Route::get('/', [CategoryController::class, 'index'])->name('categories.index');
+        Route::post('/', [CategoryController::class, 'store'])->name('categories.store');
+        Route::get('/{id}', [CategoryController::class, 'show'])->name('categories.show');
+        Route::put('/{id}', [CategoryController::class, 'update'])->name('categories.update');
+        Route::delete('/{id}', [CategoryController::class, 'destroy'])->name('categories.destroy');
+    });
+});
