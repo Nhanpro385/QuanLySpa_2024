e214615773b413bf64a8f4be888fb4afa1307d48
ThanhNhan <pronhan636@gmail.com>
2024-10-01 14:45:30 +0700

    test api

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,8 @@
-<?
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
+<?php
 
-Route::post('register', [AuthController::class, 'register']);
-Route::post('login', [AuthController::class, 'login']);
-Route::middleware('auth:api')->group(function () {
-    Route::post('logout', [AuthController::class, 'logout']);
-    Route::get('user/profile', function () {
-        return auth()->user();
-    });
-});
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
