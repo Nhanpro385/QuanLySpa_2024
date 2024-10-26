9af4ca43266053f2f177d6be24bb4bec40e345ac
duyphuong010604 <voduyphuong13@gmail.com>
2024-10-26 21:58:02 +0700

    Update Resources for Services

---
+++ app/Filters/Admin/ServiceFilter.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Filters\Admin;
+use App\Filters\ApiFilter;
+use Illuminate\Http\Request;
+class ServiceFilter extends ApiFilter
+{
+    protected $safeParams = [
+        'id' => ['eq', 'like'],
+        'name' => ['eq', 'like'],
+        'service_category_id' => ['eq', 'like'],
+        'price' => ['eq', 'gte', 'lte'],
+        'status' => ['eq']
+    ];
+
+    protected $columnMap = [
+        'id' => 'id',
+        'name' => 'name',
+        'price' => 'price',
+    ];
+
+    protected $operatorMap = [
+        'eq' => '=',
+        'lt' => '<',
+        'lte' => '<=',
+        'gt' => '>',
+        'gte' => '>=',
+        'like' => 'LIKE'
+    ];
+
+
+    protected $sortParams = [
+        'sort_by' => 'id',
+        'sort_order' => 'asc'
+    ];
+}
