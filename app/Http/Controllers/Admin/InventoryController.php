f41b2f308c9e0a17b0930f825110e5ce2e84c193
ThanhNhan <pronhan636@gmail.com>
2024-12-07 23:20:26 +0700

    fix query index inven

--- app/Http/Controllers/Admin/InventoryController.php
+++ app/Http/Controllers/Admin/InventoryController.php
@@ -6,21 +6,26 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Inventory;
 use App\Http\Resources\Admin\Inventories\InventoryResource;
+use Illuminate\Support\Facades\DB;
 
 
 
 class InventoryController extends Controller
 {
     public function index(Request $request)
-
     {
         $perPage = $request->input('perPage', 10);
-        $inventories = Inventory::with(['product', 'createdBy', 'updatedBy'])
-            ->orderBy('created_at', 'desc')
+    
+        // Lấy bản ghi mới nhất của mỗi product_id
+        $inventories = Inventory::select('inventories.*')
+            ->with(['product', 'createdBy', 'updatedBy'])
+            ->join(DB::raw('(SELECT MAX(id) as latest_id FROM inventories GROUP BY product_id) latest'), 'inventories.id', '=', 'latest.latest_id')
+            ->orderBy('inventories.created_at', 'desc')
             ->paginate($perPage);
-
+    
         return InventoryResource::collection($inventories);
     }
+    
 
     /**
      * Hiển thị chi tiết một inventory.
