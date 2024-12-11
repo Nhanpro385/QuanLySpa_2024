25dc824899139742210676dd8fc8ab0f25d68ee2
duyphuong010604 <voduyphuong13@gmail.com>
2024-12-11 17:00:33 +0700

    fix be resource Payment

--- app/Http/Resources/Admin/Payments/PaymentResource.php
+++ app/Http/Resources/Admin/Payments/PaymentResource.php
@@ -23,11 +23,11 @@ class PaymentResource extends JsonResource
             ] : null,
             'appointment_id' => $this->appointment_id ? [
                 'id' => $this->appointment->id,
-                'customer' => [
+                'customer' => $this->appointment->customer_id ? [
                     'id' => $this->appointment->customer->id,
                     'full_name' => $this->appointment->customer->full_name,
                     'phone' => $this->appointment->customer->phone
-                ]
+                ] : []
             ] : [],
             'service_total' => $this->service_total,
             'product_total' => $this->product_total,
